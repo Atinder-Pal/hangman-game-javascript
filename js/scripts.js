977279f0ee1f13e2c0f2cec913cb0a9a9a4d3c17
Atinder-Pal <atty.pal46@gmail.com>
2020-08-27 14:26:46 -0600

    added user can input and based on input chances counter and previous guesses change and word displayed changes

--- js/scripts.js
+++ js/scripts.js
@@ -19,6 +19,9 @@ console.log( previousGuesses );
 const errorMessage = document.getElementById( 'error-message' );
 console.log( errorMessage );
 
+const guessButton = document.getElementById( 'guess-button' );
+console.log( guessButton );
+
 // Citation
 //Got words from https://www.randomlists.com/random-words
 const listOfWords = ["label",
@@ -34,9 +37,10 @@ const listOfWords = ["label",
 "dispensable"];
 //End Citation
 
-const previousGuessesArray = ["d"];
+const previousGuessesArray = ["z"];
 var chancesVariable = 6;
 var arrayOfUnderscores =[];
+var randomWord = "";
 
 function getRandomWord()
 {
@@ -52,10 +56,10 @@ function getRandomWord()
 }
 //console.log(getRandomWord());
 
-function display_word()
+function displayInitial_word()
 {    
     arrayOfUnderscores =[];
-    let randomWord = getRandomWord();
+    randomWord = getRandomWord();
     let lengthOf_word = randomWord.length;
     console.log( randomWord );
     for (let i=0;i<lengthOf_word;i++)
@@ -69,28 +73,115 @@ function display_word()
     return arrayOfUnderscores;
 }
 
+function display_word(myArray)
+{
+    underscoreWord.textContent="";
+    for(const letter of myArray)
+    {
+        underscoreWord.append( letter );
+    }    
+    return myArray;
+
+}
+
 function setup()
 {
-    console.log(display_word());
+    console.log(displayInitial_word());
     //display_word();
     //previousGuessesArray.splice(0, previousGuessesArray.length);
     chancesVariable = 6;
     chances.append( chancesVariable );    
 }
+//Find indices of letter in random word
+//Citation
+//https://stackoverflow.com/questions/10710345/finding-all-indexes-of-a-specified-character-within-a-string
+function locations(substring,string)
+{
+    var a=[],i=-1;
+    while((i=string.indexOf(substring,i+1)) >= 0) a.push(i);
+    return a;
+}
+
+function replaceAt(positions, myArray, myLetter)
+{
+    for( let i of positions)
+    {
+        //Citation
+        //Replace element of an array 
+        //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/splice
+        myArray.splice(i, 1, " "+myLetter+" ");
+        //End Citation
+    }
+    return myArray;
+}
 
 setup();
 
 //Add Event Listener to Form Submission
-gameForm.addEventListener('click', (event) => {
+guessButton.addEventListener('click', (event) => {
     event.preventDefault();
-    const inputLetterValue = inputLetter.value;
+    const inputLetterValue = inputLetter.value.toLowerCase();
     console.log( inputLetterValue );
 
     inputLetter.value ="";
-    errorMessage.innerHTML="";
-    if( previousGuessesArray.includes( inputLetterValue ))
+    errorMessage.textContent="";
+    //To validate input is only alphabet
+    //Citation
+    //https://stackoverflow.com/questions/23556533/how-do-i-make-an-input-field-accept-only-letters-in-javascript
+   var alphabets = /^[a-zA-Z]/;
+   //End Citation
+
+    if( previousGuessesArray.includes( inputLetterValue )|| inputLetterValue=="" || inputLetterValue==" "|| !inputLetterValue.match(alphabets))
     {
-        errorMessage.innerHTML="You already guessed this letter, Try a different one";
+        errorMessage.textContent="You already guessed this letter, Try a different one";
+    }
+
+    else
+    {
+        previousGuessesArray.push( inputLetterValue );
+        console.log( randomWord );
+        //Check if chosenrandom word contains the input letter
+        if( randomWord.includes( inputLetterValue ) )
+        {
+            console.log("it is included in random word");
+            let indices = locations( inputLetterValue,randomWord);
+            console.log(indices);
+
+            display_word(replaceAt(indices,arrayOfUnderscores,inputLetterValue));
+            
+            previousGuesses.textContent="";
+            for(const letter of previousGuessesArray)
+            {
+                previousGuesses.append( " "+letter );
+            } 
+            if( !arrayOfUnderscores.includes(" _ "))
+            {
+                console.log("word is complete");
+            }
+        }
+
+        else
+        {
+            if(chancesVariable>-1)
+            {
+                chancesVariable--;
+                chances.textContent="";
+                chances.append( chancesVariable );
+
+                previousGuesses.textContent="";
+                for(const letter of previousGuessesArray)
+                {
+                    previousGuesses.append( " "+letter );
+                } 
+            }
+
+            if(chancesVariable ==-1)
+            {
+                console.log("chances are all used");
+            }
+            
+        }
+        console.log( previousGuessesArray );
     }
 
  });
